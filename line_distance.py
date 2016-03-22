3e0dae9e0156352f7a321a86676bb7e37e62bd32
Elfpkck <andlek@yandex.ru>
2016-03-22 10:24:20 +0300

    Added files via upload

---
+++ line_distance.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from qgis.core import *
+from qgis.utils import *
+
+layer = iface.activeLayer()
+distance_list = [
+118225,
+150593,
+165534,
+174645,
+176059,
+179604.5,
+184062,
+200730
+]
+
+for item in distance_list:
+    for line in layer.getFeatures():
+        print line.geometry().interpolate(item).asPoint()
